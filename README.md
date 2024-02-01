28b78de220e24a6fd92f1f7fc7d7e6d0b25a01de
Jan Szkaradek <js7656@gmail.com>
2024-02-01 12:01:02 +0000

    Readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # graphics-year5
+
+Opengl graphics Engine for most of my work on real time rendering and animation for my Masters. Both Mac and Windows compatible. 
+
+Thanks to cmake all you need is to build it and its dependencies and run the generated exe. (Assimp has to be built manually)
+
+```mkdir build && cd build```
+```cmake ..```
+```cmake --build . --config Debug```
+
+**For Windows:** make sure to add assimp dll and lib files into the folder where exe is built. 
--- demos/rendering1.h
+++ demos/rendering1.h
@@ -17,7 +17,9 @@
 
 void setUpScene(ImGuiWrapper* imguiWrapper){
 
-    std::string modelPath = std::string(ASSET_DIR) + "/models/teapot.fbx";
+    std::string potPath = std::string(ASSET_DIR) + "/models/teapot.fbx";
+    std::string spherePath = std::string(ASSET_DIR) + "/models/sphere.fbx";
+    std::string dragonPath = std::string(ASSET_DIR) + "/models/dragon.fbx";
     std::string planePath = std::string(ASSET_DIR) + "/models/defaultPlane.fbx";
 
     std::string vShaderPath = std::string(SRC_DIR) + "/shaders/forwardPass/phong/blinnPhong.vert";
@@ -38,12 +40,14 @@ void setUpScene(ImGuiWrapper* imguiWrapper){
     ResourceManager::addShader(toonShader);
     ResourceManager::addShader(pbrShader);
 
-    BasicMaterial *phongMaterial = new BasicMaterial(glm::vec3(1.0f,1.0f,1.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 32.0f);
-    ToonMaterial *toonMaterial = new ToonMaterial(glm::vec3(1.0f,1.0f,1.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 1.0f, 4, 0.5f);
-    BasicMaterial *floorMaterial = new BasicMaterial(glm::vec3(1.0f,1.0f,1.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 32.0f);
+    BasicMaterial *phongMaterial = new BasicMaterial(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 32.0f);
+    ToonMaterial *toonMaterial = new ToonMaterial(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 0.5f, 4, 0.019f);
+    BasicMaterial *floorMaterial = new BasicMaterial(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 32.0f);
     PBRMaterial *pbrMaterial = new PBRMaterial(glm::vec3(1.0f,1.0f,1.0f), 0.1f, 0.3f, 0.1f);
 
-    Model* model = ResourceManager::loadModel(modelPath.c_str());
+    Model* sphere = ResourceManager::loadModel(spherePath.c_str());
+    Model* pot = ResourceManager::loadModel(potPath.c_str());
+    Model* dragon = ResourceManager::loadModel(dragonPath.c_str());
     Model* plane = ResourceManager::loadModel(planePath.c_str());
 
     GameObject* player = ResourceManager::loadGameObject();
@@ -54,14 +58,14 @@ void setUpScene(ImGuiWrapper* imguiWrapper){
     ResourceManager::setActiveCamera(camera);
 
     GameObject* pot1 = ResourceManager::loadGameObject();
-    RenderModule* renderModule = new RenderModule(model, phongMaterial, shader);
+    RenderModule* renderModule = new RenderModule(pot, phongMaterial, shader);
     GameplayModule* gameplayModule = new GameplayModule();
     pot1->addModule(renderModule);
     pot1->addModule(gameplayModule);
     pot1->Translate(glm::vec3(0.0f, 0.0f, -10.0f));
 
     GameObject* pot2 = ResourceManager::loadGameObject();
-    RenderModule* renderModule1 = new RenderModule(model, toonMaterial, toonShader);
+    RenderModule* renderModule1 = new RenderModule(pot, toonMaterial, toonShader);
     GameplayModule* gameplayModule1 = new GameplayModule();
     pot2->addModule(renderModule1);
     pot2->addModule(gameplayModule1);
@@ -69,18 +73,65 @@ void setUpScene(ImGuiWrapper* imguiWrapper){
     
 
     GameObject* pot3 = ResourceManager::loadGameObject();
-    RenderModule* renderModule2 = new RenderModule(model, pbrMaterial, pbrShader);
+    RenderModule* renderModule2 = new RenderModule(pot, pbrMaterial, pbrShader);
     GameplayModule* gameplayModule2 = new GameplayModule();
     pot3->addModule(renderModule2);
     pot3->addModule(gameplayModule2);
     pot3->Translate(glm::vec3(-3.0f, 0.0f, -10.0f));
 
+    GameObject* sphere1 = ResourceManager::loadGameObject();
+    RenderModule* renderModule3 = new RenderModule(sphere, phongMaterial, shader);
+    GameplayModule* gameplayModule3 = new GameplayModule();
+    sphere1->addModule(renderModule3);
+    sphere1->addModule(gameplayModule3);
+    sphere1->Translate(glm::vec3(0.0f, 3.0f, -10.0f));
+
+    GameObject* sphere2 = ResourceManager::loadGameObject();
+    RenderModule* renderModule4 = new RenderModule(sphere, toonMaterial, toonShader);
+    GameplayModule* gameplayModule4 = new GameplayModule();
+    sphere2->addModule(renderModule4);
+    sphere2->addModule(gameplayModule4);
+    sphere2->Translate(glm::vec3(3.0f, 3.0f, -10.0f));
+    
+
+    GameObject* sphere3 = ResourceManager::loadGameObject();
+    RenderModule* renderModule5 = new RenderModule(sphere, pbrMaterial, pbrShader);
+    GameplayModule* gameplayModule5 = new GameplayModule();
+    sphere3->addModule(renderModule5);
+    sphere3->addModule(gameplayModule5);
+    sphere3->Translate(glm::vec3(-3.0f, 3.0f, -10.0f));
+
+    GameObject* dragon1 = ResourceManager::loadGameObject();
+    RenderModule* renderModule7 = new RenderModule(dragon, phongMaterial, shader);
+    GameplayModule* gameplayModule7 = new GameplayModule();
+    dragon1->addModule(renderModule7);
+    dragon1->addModule(gameplayModule7);
+    dragon1->Translate(glm::vec3(0.0f, -3.0f, -10.0f));
+    dragon1->setScale(glm::vec3(0.2f, 0.2f, 0.2f));
+
+    GameObject* dragon2 = ResourceManager::loadGameObject();
+    RenderModule* renderModule8 = new RenderModule(dragon, toonMaterial, toonShader);
+    GameplayModule* gameplayModule8 = new GameplayModule();
+    dragon2->addModule(renderModule8);
+    dragon2->addModule(gameplayModule8);
+    dragon2->Translate(glm::vec3(3.0f, -3.0f, -10.0f));
+    dragon2->setScale(glm::vec3(0.2f, 0.2f, 0.2f));
+
+    GameObject* dragon3 = ResourceManager::loadGameObject();
+    RenderModule* renderModule9 = new RenderModule(dragon, pbrMaterial, pbrShader);
+    GameplayModule* gameplayModule9 = new GameplayModule();
+    dragon3->addModule(renderModule9);
+    dragon3->addModule(gameplayModule9);
+    dragon3->Translate(glm::vec3(-3.0f, -3.0f, -10.0f));
+    dragon3->setScale(glm::vec3(0.2f, 0.2f, 0.2f));
+
+
     GameObject* floor = ResourceManager::loadGameObject();
-    RenderModule* renderModule3 = new RenderModule(plane, floorMaterial, shader);
-    floor->addModule(renderModule3);
+    RenderModule* renderModule6 = new RenderModule(plane, floorMaterial, shader);
+    floor->addModule(renderModule6);
     floor->setRotation(glm::vec3 (-90.0f, 0.0f, 0.0f));
     floor->setScale(glm::vec3(20.0f, 20.0f, 20.0f));
-    floor->Translate(glm::vec3(0.0f, -1.0f, 0.0f));
+    floor->Translate(glm::vec3(0.0f, -4.0f, 0.0f));
 
 
     player->Translate(glm::vec3(0.0f, 0.0f, 10.0f));
