e936e500fbac2769deb9f1ab519236b3a5926f4c
E1P3 <js7656@gmail.com>
2024-02-15 23:17:46 +0000

    notick

--- demos/rendering3.h
+++ demos/rendering3.h
@@ -7,8 +7,10 @@
 #include "../src/entityModules/controllerModule.h"
 #include "../src/entityModules/renderModule.h"
 #include "../src/entityModules/gameplayModule.h"
+#include "../src/materials/tessMaterial.h"
 #include "../src/materials/texturedMaterial.h"
 #include "../src/shaders/forwardPass/textured/texturedShader.h"
+#include "../src/shaders/forwardPass/tessalation/tessShader.h"
 #include "../src/imgui/imguiWrapper.h"
 
 void setUpScene(){
@@ -17,16 +19,22 @@ void setUpScene(){
     std::string spherePath = std::string(ASSET_DIR) + "/models/defaultSphere.fbx";
     std::string planePath = std::string(ASSET_DIR) + "/models/defaultPlane.fbx";
 
-    std::string vShaderPath = std::string(SRC_DIR) + "/shaders/forwardPass/textured/texturedShader.vert";
-    std::string fShaderPath = std::string(SRC_DIR) + "/shaders/forwardPass/textured/texturedShader.frag";
+    std::string vShaderPath = std::string(SRC_DIR) + "/shaders/forwardPass/tessalation/tessShader.vert";
+    std::string tcsShaderPath = std::string(SRC_DIR) + "/shaders/forwardPass/tessalation/tessShader.tesc";
+    std::string tesShaderPath = std::string(SRC_DIR) + "/shaders/forwardPass/tessalation/tessShader.tese";
+    std::string gShaderPath = std::string(SRC_DIR) + "/shaders/forwardPass/tessalation/tessShader.geom";
+    std::string fShaderPath = std::string(SRC_DIR) + "/shaders/forwardPass/tessalation/tessShader.frag";
+
+    std::string vPShaderPath = std::string(SRC_DIR) + "/shaders/forwardPass/textured/texturedShader.vert";
+    std::string fPShaderPath = std::string(SRC_DIR) + "/shaders/forwardPass/textured/texturedShader.frag";
 
     std::string rock_diffuse_path = std::string(ASSET_DIR) + "/textures/materials/rock_cliff/albedo.jpg";
     std::string rock_normal_path = std::string(ASSET_DIR) + "/textures/materials/rock_cliff/normal.jpg";
     std::string rock_height_path = std::string(ASSET_DIR) + "/textures/materials/rock_cliff/displacement.jpg";
 
-    std::string snow_diffuse_path = std::string(ASSET_DIR) + "/textures/materials/snow_mixed/albedo.jpg";
-    std::string snow_normal_path = std::string(ASSET_DIR) + "/textures/materials/snow_mixed/normal.jpg";
-    std::string snow_height_path = std::string(ASSET_DIR) + "/textures/materials/snow_mixed/displacement.jpg";
+    std::string snow_diffuse_path = std::string(ASSET_DIR) + "/textures/materials/snow_pure/albedo.jpg";
+    std::string snow_normal_path = std::string(ASSET_DIR) + "/textures/materials/snow_pure/normal.jpg";
+    std::string snow_height_path = std::string(ASSET_DIR) + "/textures/materials/snow_pure/displacement.jpg";
 
     std::string wood_diffuse_path = std::string(ASSET_DIR) + "/textures/materials/wood_bark/albedo.jpg";
     std::string wood_normal_path = std::string(ASSET_DIR) + "/textures/materials/wood_bark/normal.jpg";
@@ -35,9 +43,13 @@ void setUpScene(){
     DirectionalLight* directionalLight = ResourceManager::loadDirectionalLight(0.1f, glm::vec3(0.0f, 0.0f, 1.0f));
     PointLight* pointLight = ResourceManager::loadPointLight(0.1f, glm::vec3(-3.0f, 3.0f, -3.0f), 1.0f, 0.09f, 0.032f);
 
-    TexturedShader* rockShader = new TexturedShader(vShaderPath.c_str(), fShaderPath.c_str());
-    TexturedShader* snowShader = new TexturedShader(vShaderPath.c_str(), fShaderPath.c_str());
-    TexturedShader* woodShader = new TexturedShader(vShaderPath.c_str(), fShaderPath.c_str());
+    TexturedShader* parallaxRockShader = new TexturedShader(vPShaderPath.c_str(), fPShaderPath.c_str());
+    TexturedShader* parallaxSnowShader = new TexturedShader(vPShaderPath.c_str(), fPShaderPath.c_str());
+    TexturedShader* parallaxWoodShader = new TexturedShader(vPShaderPath.c_str(), fPShaderPath.c_str()); 
+
+    TessalationShader* rockShader = new TessalationShader(vShaderPath.c_str(), fShaderPath.c_str(), gShaderPath.c_str(), tcsShaderPath.c_str(), tesShaderPath.c_str());
+    TessalationShader* snowShader = new TessalationShader(vShaderPath.c_str(), fShaderPath.c_str(), gShaderPath.c_str(), tcsShaderPath.c_str(), tesShaderPath.c_str());
+    TessalationShader* woodShader = new TessalationShader(vShaderPath.c_str(), fShaderPath.c_str(), gShaderPath.c_str(), tcsShaderPath.c_str(), tesShaderPath.c_str());
 
     Texture* rockDiffuseTexture = ResourceManager::loadTexture(TextureType::DIFFUSE, rock_diffuse_path.c_str());
     Texture* rockNormalTexture = ResourceManager::loadTexture(TextureType::NORMAL, rock_normal_path.c_str());
@@ -63,13 +75,33 @@ void setUpScene(){
     woodShader->addTexture(woodNormalTexture);
     woodShader->addTexture(woodHeightTexture);
 
+    parallaxRockShader->addTexture(rockDiffuseTexture);
+    parallaxRockShader->addTexture(rockNormalTexture);
+    parallaxRockShader->addTexture(rockHeightTexture);
+
+    parallaxSnowShader->addTexture(snowDiffuseTexture);
+    parallaxSnowShader->addTexture(snowNormalTexture);
+    parallaxSnowShader->addTexture(snowHeightTexture);
+
+    parallaxWoodShader->addTexture(woodDiffuseTexture);
+    parallaxWoodShader->addTexture(woodNormalTexture);
+    parallaxWoodShader->addTexture(woodHeightTexture);
+
+
     ResourceManager::addShader(rockShader);
     ResourceManager::addShader(snowShader);
     ResourceManager::addShader(woodShader);
+    ResourceManager::addShader(parallaxRockShader);
+    ResourceManager::addShader(parallaxSnowShader);
+    ResourceManager::addShader(parallaxWoodShader);
 
-    TexturedMaterial *rockMaterial = new TexturedMaterial(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 32.0f, -0.012f);
-    TexturedMaterial *snowMaterial = new TexturedMaterial(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 32.0f, -0.012f);
-    TexturedMaterial *woodMaterial = new TexturedMaterial(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 32.0f, -0.012f);
+    TessalationMaterial *rockMaterial = new TessalationMaterial(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 32.0f, 0.0f, 10.0f, 5.0f);
+    TessalationMaterial *snowMaterial = new TessalationMaterial(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 32.0f, 0.0f, 10.0f, 5.0f);
+    TessalationMaterial *woodMaterial = new TessalationMaterial(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 32.0f, 0.0f, 10.0f, 5.0f);
+
+    TexturedMaterial* parallaxRockMaterial = new TexturedMaterial(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 32.0f, 0.0f);
+    TexturedMaterial* parallaxSnowMaterial = new TexturedMaterial(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 32.0f, 0.0f);
+    TexturedMaterial* parallaxWoodMaterial = new TexturedMaterial(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.5f,0.5f,0.5f), glm::vec3(1.0f,1.0f,1.0f), 32.0f, 0.0f);
 
     Model* sphereModel = ResourceManager::loadModel(spherePath.c_str());
     Model* potModel = ResourceManager::loadModel(potPath.c_str());
@@ -94,7 +126,7 @@ void setUpScene(){
     sphere->setPosition(glm::vec3(10.0f, 0.0f, 0.0f));
 
     GameObject* surface = ResourceManager::loadGameObject();
-    RenderModule* surfaceRenderModule = new RenderModule(surfaceModel, rockMaterial, rockShader);
+    RenderModule* surfaceRenderModule = new RenderModule(surfaceModel, parallaxRockMaterial, parallaxRockShader);
     surface->addModule(surfaceRenderModule);
     surface->setPosition(glm::vec3(10.0f, 0.0f, -3.0f));
     surface->setRotation(glm::vec3(0.0f, 270.0f, 0.0f));
@@ -114,7 +146,7 @@ void setUpScene(){
     sphere2->setPosition(glm::vec3(10.0f, 3.0f, 0.0f));
 
     GameObject* surface2 = ResourceManager::loadGameObject();
-    RenderModule* surface2RenderModule = new RenderModule(surfaceModel, snowMaterial, snowShader);
+    RenderModule* surface2RenderModule = new RenderModule(surfaceModel, parallaxSnowMaterial, parallaxSnowShader);
     surface2->addModule(surface2RenderModule);
     surface2->setPosition(glm::vec3(10.0f, 3.0f, -3.0f));
     surface2->setRotation(glm::vec3(0.0f, 270.0f, 0.0f));
@@ -134,7 +166,7 @@ void setUpScene(){
     sphere3->setPosition(glm::vec3(10.0f, -3.0f, 0.0f));
 
     GameObject* surface3 = ResourceManager::loadGameObject();
-    RenderModule* surface3RenderModule = new RenderModule(surfaceModel, woodMaterial, woodShader);
+    RenderModule* surface3RenderModule = new RenderModule(surfaceModel, parallaxWoodMaterial, parallaxWoodShader);
     surface3->addModule(surface3RenderModule);
     surface3->setPosition(glm::vec3(10.0f, -3.0f, -3.0f));
     surface3->setRotation(glm::vec3(0.0f, 270.0f, 0.0f));
@@ -144,6 +176,10 @@ void setUpScene(){
     ImGuiWrapper::attachGuiFunction("Rock Properties", [rockMaterial](){rockMaterial->OnGui();});
     ImGuiWrapper::attachGuiFunction("Snow Properties", [snowMaterial](){snowMaterial->OnGui();});
     ImGuiWrapper::attachGuiFunction("Wood Properties", [woodMaterial](){woodMaterial->OnGui();});
+    ImGuiWrapper::attachGuiFunction("Parallax Rock Properties", [parallaxRockMaterial](){parallaxRockMaterial->OnGui();});
+    ImGuiWrapper::attachGuiFunction("Parallax Snow Properties", [parallaxSnowMaterial](){parallaxSnowMaterial->OnGui();});
+    ImGuiWrapper::attachGuiFunction("Parallax Wood Properties", [parallaxWoodMaterial](){parallaxWoodMaterial->OnGui();});
+    ImGuiWrapper::attachGuiFunction("Point Light", [pointLight](){pointLight->OnGui();});
     
 }
 
